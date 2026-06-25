fd160d32ef09bd8b0ae7b4d7fc09edeb80ef72d1
justLearnced12 <cedrickislife@gmail.com>
2026-06-25 09:43:40 -0400

    Update index.java

--- index.java
+++ index.java
@@ -1,1 +1,14 @@
-
+import java.util;
+public class hello{
+  public static void main(String []args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Hello enter your name: ");
+    String nam = scan.nextLine();
+    if(nam.equals("cedrick")){
+      System.out.println("sup pogi:>");
+    }else{
+      System.out.println("not pogi:<");
+    }
+  }
+}
+  
